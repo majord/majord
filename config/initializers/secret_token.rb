2ba5af725466374eba8fabd5f74c068b20b200cd
Stephanie Luck <ssc.luck@gmail.com>
2012-02-17 15:41:27 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Majord::Application.config.secret_token = 'bba053ca28bb95cf195d19d68941236806a47148e5fe94411e67e0c5919512080ae3e00378d2addc47794cb87400549f302e83e36a79084541979f998c1bf23f'
